67b7ecbda92eb95c64a43c8f0abc08c570e612fd
Rico P <mailtorico@gmail.com>
2013-02-10 21:15:22 +0100

    propper loop

--- eventhelper.cpp
+++ eventhelper.cpp
@@ -2,8 +2,77 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
+#include <stdio.h>       
+#include <stdlib.h>       
 #include "eventhelper.h"
 
 XEventHelper::XEventHelper(Display* display) {
   this->xdisplay = display;   
+}
+
+void* XEventHelper::innerLoop(void* that) {
+    XEventHelper* instance = (XEventHelper*)that; 
+    int code;
+    XEvent event;
+    Display* display = instance->xdisplay; 
+    char* keyBuffer = instance->keyBuffer; 
+
+    for(int i = 0; i != MAX_KEYS; i++) {
+      keyBuffer[i] = 0;   
+    }
+
+    for(;;) {
+      XNextEvent(display, &event);
+      printf("EventType = %i\n", event.type);
+
+      switch (event.type) {
+      case ConfigureNotify:
+         //reshape(event.xconfigure.width, event.xconfigure.height);
+         break;
+      case KeyPress:
+         {
+            code = XLookupKeysym(&event.xkey, 0);
+            printf("EventCode = %i\n", code);
+            if (code < MAX_KEYS) {
+              keyBuffer[code] = 1;              
+            }
+         }
+         break;
+
+      case KeyRelease:
+         {
+            code = XLookupKeysym(&event.xkey, 0);
+            printf("EventCodeUp = %i\n", code);
+            if (code < MAX_KEYS) {
+              keyBuffer[code] = 0;
+            }
+         }
+         break;
+
+      case DestroyNotify: 
+        {
+          //We are done
+          return NULL; 
+        }
+      }
+   }
+
+   return NULL; 
+}
+
+bool XEventHelper::isKeyDown(int key) {
+    if(key >= 0 && key < MAX_KEYS) {
+      return this->keyBuffer[key];   
+    }
+
+    return 0; 
+}
+
+bool XEventHelper::isKeyUp(int key) {
+  return !isKeyDown(key); 
+} 
+
+
+void XEventHelper::startListener() {
+    pthread_create( &this->thread, NULL, &XEventHelper::innerLoop, (void*) this);  
 }
--- eventhelper.h
+++ eventhelper.h
@@ -1,7 +1,7 @@
 #ifndef _EVENTHELPER_H_ 
 #define _EVENTHELPER_H_ 
 
-//#include <pthread.h> 
+#include <pthread.h> 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
@@ -11,11 +11,17 @@
 class XEventHelper {
   private: 
     char keyBuffer[MAX_KEYS];
-
     Display* xdisplay; 
+    pthread_t thread; 
+
+    static void* innerLoop(void* that); 
 
   public: 
     XEventHelper(Display* display); 
+
+    void startListener(); 
+    bool isKeyDown(int key); 
+    bool isKeyUp(int key); 
 };
 
 #endif
